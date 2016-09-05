0e62c4f7bb4f73f37c1f8d578b1e8cd4c8b137e5
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2016-09-05 22:26:52 +0200

    v0.1.0 UXEvent adhering zepto.js
    A convencience constant for UX-Events

---
+++ build/modules/common_module.js
@@ -0,0 +1,25 @@
+export let UXEVENT = class UXEVENT {};
+UXEVENT.HOVER = "hover";
+UXEVENT.BLUR = "blur";
+UXEVENT.CHANGE = "change";
+UXEVENT.CLICK = "click";
+UXEVENT.DBLCLICK = "dblclick";
+UXEVENT.FOCUSIN = "focusin";
+UXEVENT.FOCUSOUT = "focusout";
+UXEVENT.KEYDOWN = "keydown";
+UXEVENT.KEYPRESS = "keypress";
+UXEVENT.KEYUP = "keyup";
+UXEVENT.MOUSEDOWN = "mousedown";
+UXEVENT.MOUSEENTER = "mouseenter";
+UXEVENT.MOUSELEAVE = "mouseleave";
+UXEVENT.MOUSEOUT = "mouseout";
+UXEVENT.MOUSEOVER = "mouseover";
+UXEVENT.MOUSEUP = "mouseup";
+UXEVENT.MOUSEMOVE = "mousemove";
+UXEVENT.RESIZE = "resize";
+UXEVENT.SCROLL = "scroll";
+UXEVENT.SELECT = "select";
+UXEVENT.SUBMIT = "submit";
+UXEVENT.UNLOAD = "unload";
+
+//# sourceMappingURL=common_module.js.map
